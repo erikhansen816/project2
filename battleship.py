db85ce7d7049f03fb38160abb15bef10e9e015eb
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-18 14:41:06 -0500

    Updated from Brython Server: 12/18/2017 2:41:05 PM

--- battleship.py
+++ battleship.py
@@ -1,1 +1,28 @@
+#Erik Hansen
+#12/18/17
+#battleship.py
 
+from ggame import *
+
+#Constants
+EMPTY = 0
+MISS = 1
+HIT = 2
+SQUARESIZE = 80
+
+def buildBoard():
+    
+    
+def reDrawAll():
+    for item in App().spritelist[:]:
+        item.destroy()
+     for row in range(0,5):
+        for col in range(0,5):
+            Sprite(square,(
+        print()
+
+if __name__ == '__main__':
+    black = Color(0x000000,1)
+    white = Color(0xFFFFFF,1)
+    blackOutline = LineStyle(1,black)
+    square = RectangleAsset(SQUARESIZE,SQUARESIZE,blackOutline,white)
