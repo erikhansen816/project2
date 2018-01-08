aef65d7e0bfad863138834a50484416387837327
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2018-01-07 22:11:07 -0500

    Updated from Brython Server: 1/7/2018 10:11:06 PM

--- battleship.py
+++ battleship.py
@@ -12,14 +12,14 @@ HIT = 2
 SHIP = 3
 SQUARESIZE = 50
 
-def buildBoard():
+def buildBoard(): #creates an matrix that is 5x5 and empty
     return [[EMPTY]*5,[EMPTY]*5,[EMPTY]*5,[EMPTY]*5,[EMPTY]*5]
 
 
 
 buildBoard()
     
-def reDrawAll():
+def reDrawAll(): #a function that creates the board and sprites the different color squares depending on where the ships and hits are
     for item in App().spritelist[:]:
         item.destroy()
     for row in range(0,5):
@@ -46,8 +46,8 @@ def reDrawAll():
     Sprite(TextAsset("COMPUTER", fill = black, style = "Bold 24pt Times"),(310,SQUARESIZE*5))
             
 
-def mouseClick(event):
-    if data['shipcount'] < 3:
+def mouseClick(event): 
+    if data['shipcount'] < 3: #part of function that places the players ships depending on where the click is
         if event.x <= SQUARESIZE*5 and event.y <= SQUARESIZE*5:
             pickcolumn = event.x//SQUARESIZE
             pickrow = event.y//SQUARESIZE
@@ -56,7 +56,7 @@ def mouseClick(event):
                 data['shipcount']+=1
                 reDrawAll()
     else:
-        if data['gameover'] == False:
+        if data['gameover'] == False: #part of function that will determine if you hit a ship based on the click
             if event.x >= 300 and event.x<=300+SQUARESIZE*5:
                 choosecolumn = (event.x-300)//SQUARESIZE
                 chooserow = event.y//SQUARESIZE
@@ -82,14 +82,15 @@ def mouseClick(event):
                     computer = False
                 if computer == True:
                     computerTurn()
+        #part of function that displays who won            
         if data['phits'] == 3 and data['chits']<3:
             data['gameover'] = True
-            Sprite(TextAsset("Computer wins!!!!", fill = black, style = "Bold 40pt Times"),(20,22))
+            Sprite(TextAsset("Computer wins!!!!", fill = black, style = "Bold 40pt Times"),(320,22))
         if data['chits'] == 3 and data['phits']<3:
             data['gameover'] = True
-            Sprite(TextAsset("Player wins!!!!", fill = black, style = "Bold 40pt Times"),(320,22))        
+            Sprite(TextAsset("Player wins!!!!", fill = black, style = "Bold 40pt Times"),(20,22))        
 
-def pickComputerShips():
+def pickComputerShips(): #randomly chooses three ships for the computer
     pick = False
     if pick == False:
         cships = 0
@@ -101,7 +102,7 @@ def pickComputerShips():
                 cships += 1
         pick = True
 
-def computerTurn():
+def computerTurn(): #randomly guesses where the ships are on player board
     row = randint(0,4)
     col = randint(0,4)
     if data['playerboard'][row][col] == SHIP:
@@ -117,8 +118,9 @@ def computerTurn():
         reDrawAll()
        
 
-if __name__ == '__main__':
+if __name__ == '__main__': #runs the game
     
+    #dictionary
     data = {}
     data['gameover'] = False
     data['shipcount'] = 0
@@ -127,6 +129,7 @@ if __name__ == '__main__':
     data['computerboard'] = buildBoard()
     data['playerboard'] = buildBoard()
     
+    #colors
     gray = Color(0xD3D3D3, 1)
     white = Color(0xFFFFFF, 1)
     blue = Color(0x0000FF, 1)
@@ -134,11 +137,14 @@ if __name__ == '__main__':
     black = Color(0x000000, 1)
     blackOutline = LineStyle(1,black)
     
+    #squares
     square_empty = RectangleAsset(SQUARESIZE,SQUARESIZE,blackOutline,white)
     square_miss = RectangleAsset(SQUARESIZE,SQUARESIZE,blackOutline,gray)
     square_hit = RectangleAsset(SQUARESIZE,SQUARESIZE,blackOutline,red)
     square_ship = RectangleAsset(SQUARESIZE,SQUARESIZE,blackOutline,blue)
+    
     reDrawAll()
     pickComputerShips()
+    
     App().listenMouseEvent("click", mouseClick)
     App().run()
